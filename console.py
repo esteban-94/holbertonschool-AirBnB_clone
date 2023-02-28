3d4e546b7dfe1ae9b926f3d35b3c04531796d19a
ObedRav <obedrayo2356@gmail.com>
2023-02-28 17:17:01 +0000

    The console is added

---
+++ console.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+
+    intro = "Welcome to the AIRBNB console command"
+    prompt = "(hbnb) "
+
+    def do_quit(self, args: str) -> bool:
+        return True
+
+    def do_EOF(self, args: str) -> bool:
+        return True
+    
+    def complete_add(self, text, line, start_index, end_index) -> str:
+        options = ['quit', 'help']
+        if text:
+            return [option for option in options if option.startswith(text)]
+        else:
+            return options
+
+    def default(self, line: str) -> str:
+        print(f"Command '{line}' not found, please type help to display the commands availables")
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
