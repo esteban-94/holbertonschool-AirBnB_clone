6b211ee1f4c72a56d09b8b1f2ba738ad7d00be8c
ObedRav <obedrayo2356@gmail.com>
2023-03-01 16:53:59 +0000

    Fixing some bugs

--- console.py
+++ console.py
@@ -19,39 +19,44 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, args: str) -> None:
-        # Desestructuring the args
-        class_name, *_ = args.split()
-
-        # Vallidation
-        if not class_name:
-            print("** class name missiong **")
-            return
-        if not class_name in class_names_str:
-            print("** class doesn't exist **")
-            return
+        if len(args.split()) >= 1:
+            # Desestructuring the args
+            class_name, *_ = args.split()
+            # Vallidations
+            if not class_name in class_names_str:
+                print("** class doesn't exist **")
+                return
+        else:
+            # Vallidation
+            if not args:
+                print("** class name missiong **")
+                return
 
         # Process
         for number, class_to_create in enumerate(class_names_str):
-            if args == class_to_create:
+            if class_name == class_to_create:
                 new_instance = class_names[number]()
 
         new_instance.save()
         print(new_instance.id)
 
     def do_show(self, args: str) -> None:
-        # Desestructuring the args
-        class_name, instance_id, *_ = args.split()
+        if len(args.split()) >= 2:
+            # Desestructuring the args
+            class_name, instance_id, *_ = args.split()
 
-        # Vallidations
-        if not class_name:
-            print("** class name missing **")
-            return
-        if not class_name in class_names_str:
-            print("** class doesn't exist **")
-            return
-        if not instance_id:
-            print("** instance id missing **")
-            return
+            # Vallidations
+            if not class_name in class_names_str:
+                print("** class doesn't exist **")
+                return
+        else:
+            # Vallidations
+            if not args:
+                print("** class name missing **")
+                return
+            if len(args.split()) < 2:
+                print("** instance id missing **")
+                return
 
         # Process
         model = all_data.get(f"{class_name}.{instance_id}", {})
@@ -63,13 +68,14 @@ class HBNBCommand(cmd.Cmd):
         print(model)
 
     def do_all(self, args: str) -> None:
-        # Desestructuring the args
-        class_name, *_ = args.split()
+        if args:
+            # Desestructuring the args
+            class_name, *_ = args.split()
 
-        # Validation
-        if class_name and class_name not in class_names_str:
-            print("** class doesn't exist **")
-            return
+             # Validation
+            if class_name and class_name not in class_names_str:
+                print("** class doesn't exist **")
+                return
 
         # Process
         objects = [str(obj) for obj in all_data.values() # if only write all
@@ -77,9 +83,34 @@ class HBNBCommand(cmd.Cmd):
 
         print(objects)
 
+    def do_destroy(self, args: str) -> None:
+        if len(args.split()) >= 2:
+            # Desestructuring the args
+            class_name, instance_id, *_ = args.split()
+        else:
+            # Vallidations
+            if not args:
+                print("** class name missing **")
+                return
+            if not args.split()[0] in class_names_str:
+                print("** class doesn't exist **")
+                return
+            if len(args.split()) == 1:
+                print("** instance id missing **")
+                return
+        
+        # Process
+        try:
+            all_data.pop(f"{class_name}.{instance_id}")
+        except KeyError:
+            print("** no instance found **")
+            return
+
+        print(instance_id)
+
 
     def complete_add(self, text, line, start_index, end_index) -> str:
-        options = ['quit', 'help']
+        options = ['quit', 'help', 'all', 'show', 'add']
         if text:
             return [option for option in options if option.startswith(text)]
         else:
