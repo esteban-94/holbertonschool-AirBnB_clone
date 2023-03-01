27af059dc32dde3be8c6202e0e015b4839d36e4d
ObedRav <obedrayo2356@gmail.com>
2023-03-01 19:57:42 +0000

    Updated version of the console

--- console.py
+++ console.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+"""
+Module Name: console
+Module Description:
+This module contains only one Class
+Module Classes:
+- HBNBCommand
+Module Attributes:
+- None
+"""
 import cmd
 from models.base_model import BaseModel
 from models import storage
@@ -7,98 +16,142 @@ class_names = [BaseModel]
 class_names_str = ["BaseModel"]
 all_data = storage.all()
 
+
 class HBNBCommand(cmd.Cmd):
+    """Command-line interface for the AIRBNB project."""
 
     intro = "Welcome to the AIRBNB console command"
     prompt = "(hbnb) "
 
     def do_quit(self, args: str) -> bool:
+        """Quit command to exit the program.
+
+        Args:
+            args (str): The arguments passed with the command.
+
+        Returns:
+            bool: True to exit the program.
+        """
         return True
 
     def do_EOF(self, args: str) -> bool:
+        """Handle the end-of-file event (Ctrl+D).
+
+        Args:
+            args (str): The arguments passed with the command.
+
+        Returns:
+            bool: True to exit the program.
+        """
         return True
 
     def do_create(self, args: str) -> None:
-        if len(args.split()) >= 1:
-            # Desestructuring the args
-            class_name, *_ = args.split()
-            # Vallidations
-            if not class_name in class_names_str:
-                print("** class doesn't exist **")
-                return
-        else:
-            # Vallidation
-            if not args:
-                print("** class name missiong **")
-                return
+        """Create a new instance of a given class.
+
+        Args:
+            args (str): The arguments passed with the command.
+                        It should contain the class name.
+
+        Returns:
+            None
+        """
+        arg_list = args.split()
+        if not arg_list:
+            print("** class name missing **")
+            return
+        class_name = arg_list[0]
+        if class_name not in class_names_str:
+            print("** class doesn't exist **")
+            return
 
         # Process
-        for number, class_to_create in enumerate(class_names_str):
-            if class_name == class_to_create:
-                new_instance = class_names[number]()
+        new_instance = class_names[class_names_str.index(class_name)]()
 
         new_instance.save()
         print(new_instance.id)
 
     def do_show(self, args: str) -> None:
-        if len(args.split()) >= 2:
-            # Desestructuring the args
-            class_name, instance_id, *_ = args.split()
+        """Show the string representation of an instance.
 
-            # Vallidations
-            if not class_name in class_names_str:
-                print("** class doesn't exist **")
-                return
-        else:
-            # Vallidations
-            if not args:
-                print("** class name missing **")
-                return
-            if len(args.split()) < 2:
-                print("** instance id missing **")
-                return
+        Args:
+            args (str): The arguments passed with the command.
+                        It should contain the class name and the instance ID.
+
+        Returns:
+            None
+        """
+        arg_list = args.split()
+        if not arg_list:
+            print("** class name missing **")
+            return
+
+        class_name = arg_list[0]
+
+        if class_name not in class_names_str:
+            print("** class doesn't exist **")
+            return
+        if len(arg_list) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = arg_list[1]
 
         # Process
-        model = all_data.get(f"{class_name}.{instance_id}", {})
-        
-        if model == {}:
+        model = all_data.get(f"{class_name}.{instance_id}", None)
+
+        if model is None:
             print("** no instance found **")
             return
 
         print(model)
 
     def do_all(self, args: str) -> None:
-        if args:
-            # Desestructuring the args
-            class_name, *_ = args.split()
+        """Show the string representation of all instances of a given class.
 
-             # Validation
-            if class_name and class_name not in class_names_str:
-                print("** class doesn't exist **")
-                return
+        Args:
+            args (str): The arguments passed with the command.
+                        It may contain the class name.
+
+        Returns:
+            None
+        """
+        arg_list = args.split()
+        if arg_list and arg_list[0] not in class_names_str:
+            print("** class doesn't exist **")
+            return
 
         # Process
-        objects = [str(obj) for obj in all_data.values() # if only write all
+        objects = [str(obj) for obj in all_data.values()  # if only write all
                    if args == "" or str(obj).startswith(f"[{args}]")]
 
         print(objects)
 
     def do_destroy(self, args: str) -> None:
-        if len(args.split()) >= 2:
-            # Desestructuring the args
-            class_name, instance_id, *_ = args.split()
-        else:
-            # Vallidations
-            if not args:
-                print("** class name missing **")
-                return
-            if not args.split()[0] in class_names_str:
-                print("** class doesn't exist **")
-                return
-            if len(args.split()) == 1:
-                print("** instance id missing **")
-                return
-        
+        """Delete an instance based on the class name and ID.
+
+        Args:
+            args (str): The arguments passed with the command.
+                        It should contain the class name and the instance ID.
+
+        Returns:
+            None
+        """
+        arg_list = args.split()
+        if not arg_list:
+            print("** class name missing **")
+            return
+
+        class_name = arg_list[0]
+
+        if class_name not in class_names_str:
+            print("** class doesn't exist **")
+            return
+        if len(arg_list) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = arg_list[1]
+
         # Process
         try:
             all_data.pop(f"{class_name}.{instance_id}")
@@ -106,18 +159,79 @@ class HBNBCommand(cmd.Cmd):
             print("** no instance found **")
             return
 
-        print(instance_id)
+        storage.save()
 
+    def do_update(self, args: str) -> None:
+        """Update an instance based on the class name and ID.
+
+        Args:
+            args (str): The arguments passed with the command.
+                        It should contain the class name, instance ID,
+                        attribute name, and attribute value.
+
+        Returns:
+            None
+        """
+        arg_list = args.split()
+        if not arg_list:
+            print("** class name missing **")
+            return
+
+        class_name = arg_list[0]
+
+        if class_name not in class_names_str:
+            print("** class doesn't exist **")
+            return
+        if len(arg_list) < 2:
+            print("** instance id missing **")
+            return
+
+        instance_id = arg_list[1]
+
+        instance = all_data.get(f"{class_name}.{instance_id}", None)
+
+        if instance is None:
+            print("** no instance found **")
+            return
+
+        attribute_name = arg_list[2]
+        attribute_value = arg_list[3]
+
+        setattr(instance, attribute_name, attribute_value)
+
+        instance.save()
 
     def complete_add(self, text, line, start_index, end_index) -> str:
-        options = ['quit', 'help', 'all', 'show', 'add']
+        """Provide auto-completion for some commands.
+
+        Args:
+            text (str): The current word being completed.
+            line (str): The whole command line being completed.
+            start_index (int): The start index of the current word.
+            end_index (int): The end index of the current word.
+
+        Returns:
+            str: A list of possible completions.
+        """
+        options = ['quit', 'help', 'all', 'show', 'destroy', 'update']
         if text:
             return [option for option in options if option.startswith(text)]
         else:
             return options
 
     def default(self, line: str) -> None:
-        print(f"Command '{line}' not found, please type help to display the commands availables")
+        """Handle unknown commands.
+
+        Args:
+            line (str): The unknown command.
+
+        Returns:
+            None
+        """
+        print_string = f"Command '{line}' not found, "
+        print_string += f"please type help to display the commands availables"
+        print(print_string)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
