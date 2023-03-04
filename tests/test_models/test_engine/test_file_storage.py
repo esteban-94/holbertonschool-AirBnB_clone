a103f729f55f0740f958d334cfe9ac429eae9eae
esteban <juan.esteban.94@outlook.com>
2023-03-04 16:24:28 -0500

    Some fixes in file_storage test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,62 +1,70 @@
-import unittest
+import inspect
 import json
 import os
-from models.base_model import BaseModel
-from models.city import City
+import unittest
+
 from models.engine.file_storage import FileStorage
-from models import storage
-from models.user import User
+from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
-    """ Testing File Storae functionality """
+    """ Testing the FileStorage class of the program.
+        """
 
     def setUp(self):
-        """Set up"""
-        self.file_path = 'file.json'
-        self.obj1 = BaseModel()
-        self.obj2 = User()
-        self.obj1.save()
-        self.obj2.save()
+        """ Method to prepare each single test.
+            """
+        self.storage_test = FileStorage()
+        self.path = self.storage_test._FileStorage__file_path
+        if os.path.exists(self.path):
+            os.rename(self.path, "original_{}".format(self.path))
+
+    def test_module_documentation(self):
+        """ Test if FileStorage module is documented.
+            """
+        self.assertTrue(FileStorage.__doc__)
+
+    def test_class_documentation(self):
+        """ Test if FileStorage class is documented.
+            """
+        self.assertTrue(FileStorage.__doc__)
+
+    def test_methods_documentation(self):
+        """ Test if all FileStorage methods are documented.
+            """
+        methods = inspect.getmembers(FileStorage)
+        for method in methods:
+            self.assertTrue(inspect.getdoc(method))
+
+    def test_basic_base_assigment(self):
+        """ Create some basic FileStorage instances.
+            """
+        self.assertIsInstance(self.storage_test, FileStorage)
+
+    def test_save_method(self):
+        """ Check the save() method.
+            """
+        base_model_test = BaseModel()
+        self.storage_test.save()
+        self.assertTrue(os.path.exists(self.path))
+        with open(self.path) as file:
+            file_dict = json.load(file)
+        self.assertIn(base_model_test.to_dict(), file_dict.values())
+
+    def test_reload_method(self):
+        """ Check the reload() method.
+            """
+        base_model_test = BaseModel()
+        self.storage_test.save()
+        self.storage_test.reload()
+        key_to_search = "BaseModel.{}".format(base_model_test.id)
+        file_dict = self.storage_test.all()
+        self.assertFalse(file_dict[key_to_search] is base_model_test)
 
     def tearDown(self):
-        """Tear down"""
-        os.remove(self.file_path)
-
-    def test_all(self):
-        """Test all"""
-        objs = storage.all()
-        self.assertEqual(len(objs), 22)
-        self.assertIn(f"{type(self.obj1).__name__}.{self.obj1.id}", objs)
-        self.assertIn(f"{type(self.obj2).__name__}.{self.obj2.id}", objs)
-
-    def test_new(self):
-        """Test new"""
-        obj3 = City()
-        storage.new(obj3)
-        objs = storage.all()
-        self.assertEqual(len(objs), 25)
-        self.assertIn(f"{type(obj3).__name__}.{obj3.id}", objs)
-
-    def test_save(self):
-        """Test save"""
-        with open(self.file_path, 'r') as f:
-            data = json.load(f)
-            self.assertEqual(len(data), 29)
-            self.assertIn(f"{type(self.obj1).__name__}.{self.obj1.id}", data)
-            self.assertIn(f"{type(self.obj2).__name__}.{self.obj2.id}", data)
-    
-    def test_reload(self):
-        """Test reload"""
-        with open(self.file_path, 'r') as f:
-            data = json.load(f)
-            del data[f"{type(self.obj1).__name__}.{self.obj1.id}"]
-        with open(self.file_path, 'w') as f:
-            json.dump(data, f)
-        storage.reload()
-        objs = storage.all()
-        self.assertEqual(len(objs), 27)
-
-
-if __name__ == '__main__':
-    unittest.main()
+        """ Method to leave each test
+            """
+        if os.path.exists(self.path):
+            os.remove(self.path)
+        if os.path.exists("original_{}".format(self.path)):
+            os.rename("original_{}".format(self.path), self.path)
